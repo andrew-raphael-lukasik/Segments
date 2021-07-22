4994702012a34768ce1dc4bb6f8ad6d7f69a8987
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2021-07-22 20:48:30 +0200

    mesh bounds calculations are multi-threaded now

--- Scripts/SegmentInitializationSystem.cs
+++ Scripts/SegmentInitializationSystem.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Profiling;
 using Unity.Mathematics;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
@@ -30,18 +31,44 @@ namespace Segments
 		{
 			var batches = Core.Batches;
 
+			// shedule indices job:
+			Profiler.BeginSample("shedule_indices_job");
+			int numAllIndices = 0;
+			for( int i=batches.Count-1 ; i!=-1 ; i-- )
+				numAllIndices = math.max( numAllIndices , batches[i].buffer.Length*2 );
+			var allIndices = new NativeArray<uint>( numAllIndices , Allocator.TempJob );
+			var allIndicesJobHandle = new IndicesJob{ Ptr=(uint*)allIndices.GetUnsafePtr() }.Schedule( allIndices.Length , 1024 );
+			Profiler.EndSample();
+
+			// shedule indices job:
+			Profiler.BeginSample("shedule_bounds_job");
+			NativeArray<Bounds> allBounds = new NativeArray<Bounds>( batches.Count , Allocator.TempJob );
+			NativeArray<JobHandle> allBoundsJobHandles = new NativeArray<JobHandle>( batches.Count , Allocator.Temp );
+			for( int i=batches.Count-1 ; i!=-1 ; i-- )
+			{
+				var batch = batches[i];
+				NativeArray<float3x2> buffer = batch.buffer;
+				allBoundsJobHandles[i] = new BoundsJob( buffer , allBounds , i ).Schedule();
+			}
+			Profiler.EndSample();
+
 			// remove disposed batches from the list:
+			Profiler.BeginSample("remove_disposed");
 			for( int i=batches.Count-1 ; i!=-1 ; i-- )
 				if( batches[i].isDisposed )
 					batches.RemoveAt(i);
+			Profiler.EndSample();
 			
 			// complete all dependencies:
+			Profiler.BeginSample("complete_dependencies");
 			NativeArray<JobHandle> dependencies = new NativeArray<JobHandle>( batches.Count , Allocator.Temp );
 			for( int i=batches.Count-1 ; i!=-1 ; i-- )
 				dependencies[i] = batches[i].Dependency;
 			JobHandle.CompleteAll( dependencies );
+			Profiler.EndSample();
 
 			// update vertices:
+			Profiler.BeginSample("push_vertices");
 			for( int i=batches.Count-1 ; i!=-1 ; i-- )
 			{
 				var batch = batches[i];
@@ -52,8 +79,12 @@ namespace Segments
 				mesh.SetVertexBufferParams( numVertices , _layout );
 				mesh.SetVertexBufferData( buffer , 0 , 0 , buffer.Length );
 			}
+			Profiler.EndSample();
+
+			allIndicesJobHandle.Complete();
 
 			// update indices:
+			Profiler.BeginSample("push_indices");
 			for( int i=batches.Count-1 ; i!=-1 ; i-- )
 			{
 				var batch = batches[i];
@@ -61,42 +92,76 @@ namespace Segments
 				Mesh mesh = batch.mesh;
 				int numVertices = buffer.Length * 2;
 				int numIndices = numVertices;
-				var indices = new NativeArray<uint>( numIndices , Allocator.TempJob );
-				var indicesJob = new IndicesJob{ Ptr=(uint*)indices.GetUnsafePtr() }.Schedule( indices.Length , 1024 );
+				var indices = AsArray<int>( allIndices.GetUnsafePtr() , numIndices );
 				
 				mesh.SetIndexBufferParams( numIndices , IndexFormat.UInt32 );
-				
-				indicesJob.Complete();
 				mesh.SetIndexBufferData( indices , 0 , 0 , numIndices );
-				indices.Dispose();
-
-				if( mesh.GetSubMesh(0).indexCount!=numIndices )
-				{
-					mesh.SetSubMesh(
-						index:	0 ,
-						desc:	new SubMeshDescriptor( indexStart:0 , indexCount:numIndices , topology:MeshTopology.Lines ) ,
-						flags:	MeshUpdateFlags.DontValidateIndices
-					);
-				}
+				mesh.SetSubMesh( 0 , new SubMeshDescriptor( indexStart:0 , indexCount:numIndices , topology:MeshTopology.Lines ) );
 			}
+			Profiler.EndSample();
 
-			// final updates:
+			JobHandle.CompleteAll( allBoundsJobHandles );
+
+			// push bounds:
+			Profiler.BeginSample("push_bounds");
 			for( int i=batches.Count-1 ; i!=-1 ; i-- )
-			{
-				Mesh mesh = batches[i].mesh;
-				mesh.RecalculateBounds();
-				mesh.UploadMeshData( false );
-			}
+				batches[i].mesh.bounds = allBounds[i];
+			Profiler.EndSample();
+
+			// // upload mesh data:
+			// Profiler.BeginSample("upload_mesh_data");
+			// for( int i=batches.Count-1 ; i!=-1 ; i-- )
+			// 	batches[i].mesh.UploadMeshData( false );// this seems to do nothing here (?)
+			// Profiler.EndSample();
+
+			allIndices.Dispose();
+			allBounds.Dispose();
 		}
 
-
-		[BurstCompile]
-		public unsafe struct IndicesJob : IJobParallelFor
+		public unsafe NativeArray<T> AsArray <T> ( void* dataPtr , int dataLength ) where T : unmanaged
 		{
-			[NativeDisableUnsafePtrRestriction][WriteOnly] public uint* Ptr;
-			void IJobParallelFor.Execute ( int index ) => Ptr[index] = (uint) index;
+			var nativeArray = NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<T>( dataPtr , dataLength , Allocator.None );
+			#if ENABLE_UNITY_COLLECTIONS_CHECKS
+			NativeArrayUnsafeUtility.SetAtomicSafetyHandle( ref nativeArray , AtomicSafetyHandle.Create() );
+			#endif
+			return nativeArray;
 		}
 
 
 	}
+
+
+	[BurstCompile]
+	unsafe struct IndicesJob : IJobParallelFor
+	{
+		[NativeDisableUnsafePtrRestriction][WriteOnly] public uint* Ptr;
+		void IJobParallelFor.Execute ( int index ) => Ptr[index] = (uint) index;
+	}
+
+
+	[BurstCompile]
+	unsafe struct BoundsJob : IJob
+	{
+		[NativeDisableUnsafePtrRestriction][ReadOnly] float3x2* InputPtr;
+		[ReadOnly] int InputLength;
+		[NativeDisableUnsafePtrRestriction][WriteOnly] Bounds* OutputPtr;
+		public BoundsJob ( NativeArray<float3x2> input ,  NativeArray<Bounds> output , int outputIndex )
+		{
+			this.InputPtr = (float3x2*) input.GetUnsafePtr();
+			this.InputLength = input.Length;
+			this.OutputPtr = ((Bounds*) output.GetUnsafePtr())+outputIndex;
+		}
+		void IJob.Execute ()
+		{
+			MinMaxAABB combined = MinMaxAABB.Empty;
+			for( int i=InputLength-1 ; i!=-1 ; i-- )
+			{
+				combined.Encapsulate( InputPtr[i].c0 );
+				combined.Encapsulate( InputPtr[i].c1 );
+			}
+			*OutputPtr = new Bounds{ min=combined.Min , max=combined.Max };
+		}
+	}
+
+	
 }
