0238d84a63abbef7e6d6535a6efa78f45818cfb8
andrew-raphael-lukasik <andrew.r.lukasik@gmail.com>
2025-08-16 23:25:35 +0200

    added: 2 constructors for `Segment` component

--- Scripts/Segment.cs
+++ Scripts/Segment.cs
@@ -6,5 +6,7 @@ namespace Segments
     public struct Segment : IBufferElementData
     {
         public float3x2 Value;
+        public Segment (float3 a, float3 b) => this.Value = new float3x2(a, b);
+        public Segment (float3x2 ab) => this.Value = ab;
     }
 }
