f286ddc5d3abe1f31cc79f710c9ef9e1c9b668e4
andrew-raphael-lukasik <andrew.r.lukasik@gmail.com>
2025-08-17 18:45:05 +0200

    added implicit `Segment` <-> `float3x2` conversion

--- Scripts/Segment.cs
+++ Scripts/Segment.cs
@@ -6,7 +6,11 @@ namespace Segments
     public struct Segment : IBufferElementData
     {
         public float3x2 Value;
+        
         public Segment (float3 a, float3 b) => this.Value = new float3x2(a, b);
         public Segment (float3x2 ab) => this.Value = ab;
+        
+        public static implicit operator Segment ( float3x2 value ) => new Segment{Value = value};
+        public static implicit operator float3x2 ( Segment value ) => value.Value;
     }
 }
