3a8bbe2017f2c6153318f38ee1f372dfbcb02b91
andrew-raphael-lukasik <andrew.r.lukasik@gmail.com>
2025-08-20 09:59:12 +0200

    removed `StressTestCopyEditorStateSystem`

--- Samples/Stress-Tests/300_000-Animated-Segments/StressTestAuthoring.cs
+++ Samples/Stress-Tests/300_000-Animated-Segments/StressTestAuthoring.cs
@@ -27,10 +27,34 @@ namespace Samples
             entityManager.AddComponentData(_segments, new LocalToWorld{
                 Value = transform.localToWorldMatrix
             } );
-            entityManager.AddComponentObject(_segments, this);// adds this StressTestsAuthoring component as a managed obj reference
         }
 
         void OnDisable() => Segments.Core.Destroy(_segments);
+
+        void Update()
+        {
+            // update transform (because lines are in local-space):
+            var entityManager = Segments.Core.GetWorld().EntityManager;
+            entityManager.SetComponentData(_segments, new LocalToWorld{
+                Value = transform.localToWorldMatrix
+            });
+        }
+
+        #if UNITY_EDITOR
+        void OnValidate()
+        {
+            // update settings on inspector change:
+            var entityManager = Segments.Core.GetWorld().EntityManager;
+            if (entityManager.Exists(_segments))
+            {
+                entityManager.SetComponentData(_segments, new StressTestSettings{
+                    numSegments = _numSegments,
+                    frequency   = _frequency,
+                    everyFrame  = _everyFrame ? (byte)1 : (byte)0,
+                });
+            }
+        }
+        #endif
 
         #if UNITY_EDITOR
         void OnDrawGizmos() => Gizmos.DrawIcon(transform.position, "");// draws a white square icon to help with object selection in Scene view
